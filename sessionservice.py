eda28b4438d2ebd3d74f12483980becd9f6ae9f7
Song Yang <songyang1218@gmail.com>
2019-12-12 12:19:15 -0500

    Change to two table strategy

--- sessionservice.py
+++ sessionservice.py
@@ -3,7 +3,7 @@ import json
 
 class PlayerSessionService(object):
 
-    YEAR_IN_SECONDS = 31556952
+    YEAR_IN_SECONDS = 31556952 # time to live (TTL)
 
     def __init__(self, keyspace, cluster_address=None):
         """ Initializes a service that consumes player sessions from a Cassandra cluster.
@@ -12,6 +12,7 @@ class PlayerSessionService(object):
                 cluster_address=None : an array of IP addresses to cluster nodes
         """
 
+        # May need to authenticate
         if cluster_address is not None:
             self.cluster = Cluster(cluster_address)
         else:
@@ -21,23 +22,51 @@ class PlayerSessionService(object):
         self.session = self.cluster.connect()
         self.session.execute("""
             CREATE KEYSPACE IF NOT EXISTS %s
-            WITH replication = {'class' : 'SimpleStrategy', 'replication_factor' : 3};
+            WITH replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1};
             """ % (keyspace))
         self.session.execute("USE %s;" % (keyspace))
         
-        # Initialize a table called <keyspace>_table
-        self.table = keyspace + "_table"
-        self.session.execute("DROP TABLE %s;" % (self.table))
+        # The strategy to avoid sorting after querying is to use two tables.
+
+        # In exchange for higher memory, the query time will be reduced.
+        # Cassandra's compaction algorithm is used to clean up the second, temporary table
+        # to partially mitigate the memory consumption, assuming a session lasts shorter
+        # than the tombstone time period (10 days by default). The drawback can use at most
+        # twice as much memory than the amount of received data.
+
+        # Initialize a table called <keyspace>_completed
+        # The completed table contains completed sessions ordered by end time for each player
+        # This is the only table queried by the API fetch function
+        self.completed = keyspace + "_completed"
         self.session.execute("""
-            CREATE TABLE IF NOT EXISTS %s (
-                event text,
-                country text,
-                player_id text,
+            CREATE TABLE IF NOT EXISTS %s(
                 session_id uuid,
-                ts text,
-                PRIMARY KEY (player_id)
+                player_id ascii,
+                country text,
+                start_time timestamp,
+                end_time timestamp,
+                PRIMARY KEY((player_id), end_time)
+            )
+            WITH CLUSTERING ORDER BY(end_time DESC);
+            """ % (self.completed))
+        
+        # Initialize the secondary, temporary table called <keyspace>_incomplete
+        # The incomplete table contains event as they come in from the API receive function
+        # If two events are matched, a session is completed and is then inserted into the
+        # completed table.
+        self.incomplete = keyspace + "_incomplete"
+        self.session.execute("""
+            CREATE TABLE IF NOT EXISTS %s(
+                player_id ascii,
+                session_id uuid,
+                country text,
+                start_time timestamp,
+                end_time timestamp,
+                PRIMARY KEY(player_id, session_id)
             );
-            """ % (self.table))
+        """ % (self.incomplete))
+
+        
     
     def receive_events(self, sessions):
         """ Receives a batch of events in encoded json objects to insert into the database
@@ -46,7 +75,7 @@ class PlayerSessionService(object):
 
         # verify batch size
         if len(sessions) == 0 or len(sessions) > 10:
-            raise AttributeError("Batch size is 0 or greater than 10")
+            raise AttributeError("Batch size is 0 or greater than 10: size %d" % (len(sessions)))
 
         # convert json encoding to python
         for session in sessions:
@@ -63,45 +92,45 @@ class PlayerSessionService(object):
                 if key not in ['event', 'country', 'player_id', 'session_id', 'ts']:
                     raise AttributeError("JSON object has incompatible key: %s" % (key))
             
-            
+            # Insert into incomplete table
             cql_request = ""
             
-            # if country is a key in an end event, the country field is thought as null
-            if event_type is "end":
+            # Start event
+            if event_type == "start":
                 cql_request = """
-                INSERT INTO %s (
-                    event, player_id, session_id, ts
-                ) 
-                VALUES(
-                    '%s', '%s', %s, '%s'
-                )
-                USING TTL %d;
-                """ % (self.table,
-                decoded['event'], # no country
-                decoded['player_id'], # as string
-                decoded['session_id'],
-                decoded['ts'],
-                PlayerSessionService.YEAR_IN_SECONDS) # data expires after one year
-            
-            # start request
+                    INSERT INTO %s (player_id, session_id, country, start_time) 
+                    VALUES ('%s', %s, %s, '%s')
+                    IF NOT EXISTS
+                    USING TTL %d;
+                    """ % (self.incomplete,
+                    decoded['player_id'],
+                    decoded['session_id'],
+                    decoded['country'],
+                    decoded['start_time'].replace('T', " ")[:-3], # format timestamp
+                    PlayerSessionService.YEAR_IN_SECONDS)
+
+            # End event
             else:
                 cql_request = """
-                INSERT INTO %s (
-                    event, country, player_id, session_id, ts
-                ) 
-                VALUES(
-                    '%s', '%s', '%s', %s, '%s'
-                )
-                USING TTL %d;
-                """ % (self.table,
-                decoded['event'], decoded['country'],
-                decoded['player_id'], # as string
-                decoded['session_id'],
-                decoded['ts'],
-                PlayerSessionService.YEAR_IN_SECONDS) # data expires after one year
-                
+                    INSERT INTO %s (player_id, session_id, end_time) 
+                    VALUES ('%s', %s, '%s')
+                    IF NOT EXISTS
+                    USING TTL %d;
+                    """ % (self.incomplete,
+                    decoded['player_id'],
+                    decoded['session_id'],
+                    decoded['end_time'].replace('T', " ")[:-3], # format timestamp
+                    PlayerSessionService.YEAR_IN_SECONDS)
+
             # insert the entry
-            self.session.execute(cql_request)
+            self.session.execute_async(cql_request) # asynchronous
+
+    def fetch(self, player_id):
+        """ Returns the last 20 completed sessions for the given player
+        """
+
+
+        pass
             
 
 
