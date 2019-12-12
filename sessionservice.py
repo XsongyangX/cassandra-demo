808062bd2fb524565c5f7df6d987bf53e09f3fa8
Song Yang <songyang1218@gmail.com>
2019-12-11 23:54:06 -0500

    Complete simple receiver function

--- sessionservice.py
+++ sessionservice.py
@@ -3,6 +3,8 @@ import json
 
 class PlayerSessionService(object):
 
+    YEAR_IN_SECONDS = 31556952
+
     def __init__(self, keyspace, cluster_address=None):
         """ Initializes a service that consumes player sessions from a Cassandra cluster.
             Arguments:
@@ -24,16 +26,18 @@ class PlayerSessionService(object):
         self.session.execute("USE %s;" % (keyspace))
         
         # Initialize a table called <keyspace>_table
+        self.table = keyspace + "_table"
+        self.session.execute("DROP TABLE %s;" % (self.table))
         self.session.execute("""
             CREATE TABLE IF NOT EXISTS %s (
                 event text,
                 country text,
-                player_id bigint,
-                session_id bigint,
-                ts date,
+                player_id text,
+                session_id uuid,
+                ts text,
                 PRIMARY KEY (player_id)
             );
-            """ % (keyspace + "_table"))
+            """ % (self.table))
     
     def receive_events(self, sessions):
         """ Receives a batch of events in encoded json objects to insert into the database
@@ -58,9 +62,47 @@ class PlayerSessionService(object):
                     event_type = "start"
                 if key not in ['event', 'country', 'player_id', 'session_id', 'ts']:
                     raise AttributeError("JSON object has incompatible key: %s" % (key))
-            # if country is a key in an end event, the country field is thought as null
             
-
+            
+            cql_request = ""
+            
+            # if country is a key in an end event, the country field is thought as null
+            if event_type is "end":
+                cql_request = """
+                INSERT INTO %s (
+                    event, player_id, session_id, ts
+                ) 
+                VALUES(
+                    '%s', '%s', %s, '%s'
+                )
+                USING TTL %d;
+                """ % (self.table,
+                decoded['event'], # no country
+                decoded['player_id'], # as string
+                decoded['session_id'],
+                decoded['ts'],
+                PlayerSessionService.YEAR_IN_SECONDS) # data expires after one year
+            
+            # start request
+            else:
+                cql_request = """
+                INSERT INTO %s (
+                    event, country, player_id, session_id, ts
+                ) 
+                VALUES(
+                    '%s', '%s', '%s', %s, '%s'
+                )
+                USING TTL %d;
+                """ % (self.table,
+                decoded['event'], decoded['country'],
+                decoded['player_id'], # as string
+                decoded['session_id'],
+                decoded['ts'],
+                PlayerSessionService.YEAR_IN_SECONDS) # data expires after one year
+                
+            # insert the entry
+            self.session.execute(cql_request)
+            
 
 
 if __name__ == "__main__":
@@ -75,16 +117,16 @@ if __name__ == "__main__":
             {
                 "event":"start",
                 "country":"CA",
-                "player_id":"10e123",
-                "session_id":"10a23",
+                "player_id":"0a2d12a1a7e145de8bae44c0c6e06629",
+                "session_id":"4a0c43c9-c43a-42ff-ba55-67563dfa35d4",
                 "ts":"2016-12-02T12:48:05.520022"
             }
         """
         e2 = """
             {
                 "event":"end",
-                "player_id":"10e123",
-                "session_id":"10a23",
+                "player_id":"0a2d12a1a7e145de8bae44c0c6e06629",
+                "session_id":"4a0c43c9-c43a-42ff-ba55-67563dfa35d4",
                 "ts":"2016-12-02T12:49:05.520022"
             }
         """
